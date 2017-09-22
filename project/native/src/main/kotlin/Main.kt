e88ffd9985aedb871676ba42a8cd5ce6b2bb60f9
Brent Watson <brentw@vimeo.com>
2017-09-22 10:12:35 -0400

    Update native code to choose random entry

--- project/native/src/main/kotlin/Main.kt
+++ project/native/src/main/kotlin/Main.kt
@@ -1,7 +1,17 @@
 import kotlinx.cinterop.*
 import stdio.*
+import time.*
+
+/**
+ * - Compile with `gradlew build`
+ * - Run from `build/konan/bin`
+ */
 
 fun main(args: Array<String>) {
+    if(args.size == 0) {
+        println("Input file expected as arg 1")
+        return
+    }
     val fileName = args[0]
     val file = fopen(fileName, "r")
     if (file == null) {
@@ -24,6 +34,10 @@ fun main(args: Array<String>) {
     } finally {
         fclose(file)
     }
+//    println(lines)
 
-    println(lines)
+    srand(time(null).toInt())
+    val randIndex = rand() % lines.size
+    println(randIndex)
+    println(lines[randIndex])
 }
